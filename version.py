e4879ea0bf96a242507bbe53378924788f22b04c
Stan Livitski <ved.github@livitski.com>
2019-01-17 18:51:12 -0500

    Apply the license unform with other components of enclosing project

--- version.py
+++ version.py
@@ -1,9 +1,18 @@
 # vim:fileencoding=UTF-8 
 #
-# Copyright © 2015, 2018 Stan Livitski
+# Copyright © 2015, 2016, 2018 Stan Livitski
 #     
-# Published under MIT License. See `LICENSE` file at the
-# root of this repository for details.
+#    Licensed under the Apache License, Version 2.0 with modifications,
+#  (the "License"); you may not use this file except in compliance
+#  with the License. You may obtain a copy of the License at
+#
+#   https://raw.githubusercontent.com/StanLivitski/EPyColl/master/LICENSE
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 #
 """
     Helpers that provide Python version information and runtime
